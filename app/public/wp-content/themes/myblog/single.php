a966e27cf0e0db68ae0a4e2e87d3a65d7ef0a339
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-09-07 17:02:36 +0900

    Update_アイキャッチイメージ設定

--- app/public/wp-content/themes/myblog/single.php
+++ app/public/wp-content/themes/myblog/single.php
@@ -14,7 +14,15 @@
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <!-- Page Header -->
-      <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+      <?php
+      if (has_post_thumbnail()) :
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id, 'full');
+      else :
+        $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
+      endif;
+      ?>
+      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
         <div class="overlay"></div>
         <div class="container">
           <div class="row">
@@ -35,6 +43,7 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(); ?>
               <?php the_content(); ?>
             </div>
           </div>
