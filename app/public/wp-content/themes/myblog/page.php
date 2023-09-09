840f4c2a2916a917a503d26f5fa255c57a1a499c
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-09-09 21:37:11 +0900

    create_page.php

---
+++ app/public/wp-content/themes/myblog/page.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+  <?php get_header(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+  <?php get_template_part('includes/header'); ?>
+
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <!-- Page Header -->
+      <header class="masthead" style="background-image: url('img/about-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="page-heading">
+                <h1><?php the_title(); ?></h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <!-- Main Content -->
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+    <?php endwhile; ?>
+  <?php endif; ?>
+
+  <?php get_template_part('includes/footer'); ?>
+
+  <?php get_footer(); ?>
+
+</body>
+
+</html>
